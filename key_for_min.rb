fa414d6291bc691c4aeca395dcb06e54aa1c5452
Melissa Christman <melchrstmn@gmail.com>
2019-10-17 17:12:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,3 +12,17 @@ def key_for_min_value(name_hash)
   end
   lowest_key
 end
+
+def key_for_min_value(name_hash)
+  count = 0
+  lowest_key = nil
+  name_hash.each do |key, value|
+    if count == 0
+      count = value; lowest_key = key
+      else nil 
+    if count > value 
+      count = value; lowest_key = key
+      else nil
+  end
+  lowest_key
+end 
